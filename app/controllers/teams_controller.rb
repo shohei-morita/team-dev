d8785fff080bea60647106787eaa29f7b2af5aad
shohei morita <shomorita1985@gmail.com>
2020-07-04 20:20:49 +0900

    権限移動機能実装

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_team, only: %i[show edit update destroy]
+  before_action :set_team, only: %i[show edit update destroy give_authority]
 
   def index
     @teams = Team.all
@@ -47,6 +47,14 @@ class TeamsController < ApplicationController
     @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
   end
 
+  def give_authority
+    if @team.update(team_authority_params)
+      redirect_to team_url, notice: 'リーダー権限を移動しました！'
+    else
+      redirect_to team_url, notice: 'リーダー権限の移動に失敗しました'
+    end
+  end
+
   private
 
   def set_team
@@ -55,5 +63,9 @@ class TeamsController < ApplicationController
 
   def team_params
     params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
+  end
+
+  def team_authority_params
+    params.fetch(:team, {}).permit %i[name icon icon_cache owner_id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,11 @@ Rails.application.routes.draw do
     passwords: 'users/passwords'
   }
   resource :user
-  
+
   resources :teams do
+    member do
+      patch :give_authority
+    end
     resources :assigns, only: %w(create destroy)
     resources :agendas, shallow: true do
       resources :articles do
